ce5c2bdc044e56bcd50c5eda5585d5f623bf860f
Christopher S. Corley <cscorley@gmail.com>
2019-02-02 17:40:53 -0500

    Add version bumping to heroku releases

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page. Version {{ version }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "about",
+  data: function() {
+    return {
+      version: process.env.VUE_APP_VERSION
+    };
+  }
+};
+</script>
